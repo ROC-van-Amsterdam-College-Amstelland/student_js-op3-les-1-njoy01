a5504b76b934fb233710bb4789b8df89125a5d68
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-02-06 10:45:03 +0100

    opdracht 1

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -4,6 +4,9 @@ var go = "groen";
 
 window.onload = function(){
     //JOUW CODE HIER
+     document.getElementById("light-top").innerHTML=stop;
+     document.getElementById("light-middle").innerHTML=maybe;
+     document.getElementById("light-bottom").innerHTML=go;
 
     
 }
